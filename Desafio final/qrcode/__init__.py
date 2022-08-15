cfc9c261cdf056148ce4a8f2943f0fc813a325bf
Bruno Maia <36906118+BrunoMaia@users.noreply.github.com>
2022-08-14 22:36:55 -0300

    Delete Desafio final/qrcode directory
    Para evitar o erro, devido ao nome ser igual do módulo importado

--- Desafio final/qrcode/__init__.py
+++
@@ -1,8 +0,0 @@
-import qrcode
-import datetime
-
-def CriaQRCODE(link:str):
-    agora = datetime.datetime.now()
-    QrcodeImagem = qrcode.make(link)
-def SalvaQRCODE(imagem):
-    QrcodeImagem.save(agora.strftime("qr_%d%m%y_%H%M%S"))
