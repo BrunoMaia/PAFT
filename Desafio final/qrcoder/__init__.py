a367c049fb8d3665c2563dff0e048d394abc9872
Bruno Maia <36906118+BrunoMaia@users.noreply.github.com>
2022-08-15 16:25:51 -0300

    Update __init__.py

--- Desafio final/qrcoder/__init__.py
+++ Desafio final/qrcoder/__init__.py
@@ -1,9 +1,13 @@
 import qrcode
 import datetime
 
-def CriaQRCODE(link:str):
-    agora = datetime.datetime.now()
-    agora = agora.strftime("qr_%d%m%y_%H%M%S")
-    QrcodeImagem = qrcode.make(link)
-    QrcodeImagem.save('./qrcodes/' + agora)
-    return './qrcodes/'+agora
+def CriaQRCODE(Link:str):
+    """Essa função cria um QRcode e salva no disco, a partir de uma string, que espera-se ser um link
+    :Link -> uma string com um link a ser criado o QRcode
+    :retorno -> retorna o caminho do qrcode criada, é uma string
+    """
+    Agora = datetime.datetime.now()
+    Agora = Agora.strftime("qr_%d%m%y_%H%M%S")
+    QrcodeImagem = qrcode.make(Link)
+    QrcodeImagem.save('./qrcodes/' + Agora)
+    return './qrcodes/' + Agora
