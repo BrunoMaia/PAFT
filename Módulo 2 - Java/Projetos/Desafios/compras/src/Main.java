42309b5e5306af11ecaf3998222d1a1a55d01f6f
Bruno Maia <bruno@archpc.localdomain>
2022-09-11 22:17:38 -0300

    Finalizado menu de compras, totalmente funcional :D

--- Módulo 2 - Java/Projetos/Desafios/compras/src/Main.java
+++ Módulo 2 - Java/Projetos/Desafios/compras/src/Main.java
@@ -1,19 +1,7 @@
-import businessLogic.Usuario;
 import cli.Cli;
-
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Usuario usuario_logado = null;
-        Cli.exibeBoasVindas();
-        do {
-            usuario_logado = Cli.fazLogin();
-        }while (usuario_logado == null);
-        int opcao = Cli.exibeMenu();
-        if (opcao == 5){
-            Cli.exibeSobre();
-            opcao = Cli.exibeMenu();
-        }
+        new Cli().executarCli();
     }
 }
--- Módulo 2 - Java/Projetos/Desafios/compras/src/businessLogic/BusinessLogic.java
+++ Módulo 2 - Java/Projetos/Desafios/compras/src/businessLogic/BusinessLogic.java
@@ -1,7 +1,17 @@
 package businessLogic;
 
+import businessLogic.system.Arquivo;
+import businessLogic.system.Compra;
+import businessLogic.system.Produto;
+import businessLogic.system.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BusinessLogic {
-    private Arquivo usuarios = new Arquivo("usuarios.csv",false);
+    private List<Usuario> usuarios = new Arquivo("usuarios.csv").carregaUsuarios();
+    private List<Produto> produtosCadastrados = new Arquivo("produtos.csv").carregaProdutos();
+    private float quantidadeVendas = 0;
     public static final int TAMANHO_CPF = 11;
 
     public Usuario logarUsuario(String nome, String senha){
@@ -12,9 +22,45 @@ public class BusinessLogic {
         if (nome.equals("admin") && senha.equals("admin")){
             return new Usuario(nome,true);
         }
-        if(usuarios.existeUsuario(nome, senha.hashCode())){
-            return new Usuario(nome, senha.hashCode());
+        Usuario usuarioTeste = new Usuario(nome,senha);
+        if(usuarios.contains(usuarioTeste)){
+            return usuarioTeste;
         }
         return null;
+    }
+
+    public List<Produto> buscarProduto(String nomeProduto){
+        List<Produto> resultado = new ArrayList<>();
+        for (Produto produto : produtosCadastrados){
+            if (produto.getNome().contains(nomeProduto) && produto.getEstoque() > 0){
+                resultado.add(produto);
+            }
+        }
+        return resultado;
+    }
+
+    public List<Produto> getProdutos(){
+        return produtosCadastrados;
+    }
+
+    public boolean adicionarAoCarrinho(int item, int quantidade, Usuario usuario){
+        Produto teste = new Produto(item, "Teste");
+        if (produtosCadastrados.contains(teste)){
+            int i = produtosCadastrados.indexOf(teste);
+            teste = produtosCadastrados.get(i);
+            Compra carrinho = usuario.getCarrinho();
+            carrinho.adcionaProduto(teste,quantidade);
+            return true;
+        }
+
+        return false;
+    }
+
+    public void finalizarCompra(Usuario usuario){
+        Compra carrinhoUsuario = usuario.getCarrinho();
+        carrinhoUsuario.fechaCompra();
+        usuario.addCompra(carrinhoUsuario);
+        usuario.limpaCarrinho();
+        quantidadeVendas += carrinhoUsuario.getCustoTotal();
     }
 }
